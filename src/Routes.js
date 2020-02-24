2f909daee14fb338ec7c98c538665f2774f946fd
zu-hwang <tropical.kitsch@gmail.com>
2020-02-24 21:31:49 +0900

    login ,register form html/css complited

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./Pages/Login/Login";
+import Register from "./Pages/Register/Register";
 
 class Routes extends React.Component {
 	render() {
 		return (
 			<Router>
 				<Switch>
-					<Route exact path="/login" component={Login} />
+					<Route exact path='/login' component={Login} />
+					<Route exact path='/register' component={Register} />
 				</Switch>
 			</Router>
 		);
