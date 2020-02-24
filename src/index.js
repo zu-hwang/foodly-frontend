84d98b5e54bcc64dd3644a863ff6e1bb167829a4
최예지 <yeji@choeyejiui-MacBookPro.local>
2020-02-24 20:15:58 +0900

    출동해결

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './Styles/index.css';
-import './Styles/reset.css';
-import Routes from './Routes';
-
-ReactDOM.render(<Routes />, document.getElementById('root'));
