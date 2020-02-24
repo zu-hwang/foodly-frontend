9f927b80b0f248625131341428b37940abae3cb6
zu-hwang <tropical.kitsch@gmail.com>
2020-02-24 19:29:41 +0900

    modified css > scss

---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import LoginForm from '../../Components/LoginForm/LoginForm';
+
+class Login extends Component {
+	render() {
+		return (
+			<>
+				<LoginForm />
+			</>
+		);
+	}
+}
+
+export default Login;
--- src/Routes.js
+++ src/Routes.js
@@ -1,23 +1,16 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./Pages/Login/Login";
 
 class Routes extends React.Component {
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Main} />
-          <Route exact path="/product" component={Product} />
-          <Route exact path="/product/detail" component={Detail} />
-          <React exact path="/login" component={Login} />
-          <React exact path="/register" component={Register} />
-        </Switch>
-      </Router>
-    )
-  }
+	render() {
+		return (
+			<Router>
+				<Switch>
+					<Route exact path="/login" component={Login} />
+				</Switch>
+			</Router>
+		);
+	}
 }
 export default Routes;
