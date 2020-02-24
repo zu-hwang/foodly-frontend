9287e18a765e63f642b7612e705df695b67bdbbb
kwonki <kihyeon8949@gmail.com>
2020-02-24 19:29:54 +0900

    추가사항

---
+++ src/Components/LogoContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class LogoContainer extends React.Component {
+
+    render(){
+        return( 
+          <div className = "header-container">
+            asdfsdfasfsafasfsa
+          </div>
+        )
+    }
+}
+
+export default LogoContainer;
--- src/Routes.js
+++ src/Routes.js
@@ -4,20 +4,23 @@ import {
   Route,
   Switch
 } from 'react-router-dom';
+import Nav from './Pages/Header/Nav';
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Main} />
+          {/* <Route exact path="/" component={Main} />
           <Route exact path="/product" component={Product} />
           <Route exact path="/product/detail" component={Detail} />
           <React exact path="/login" component={Login} />
-          <React exact path="/register" component={Register} />
+          <React exact path="/register" component={Register} /> */}
+          <Route exact path="/Nav" component={Nav} />
         </Switch>
       </Router>
     )
   }
 }
+
 export default Routes;
