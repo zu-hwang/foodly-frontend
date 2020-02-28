0017c32bfc0b5ccd7038adb2272d309b1336aa1b
kwonki <kihyeon8949@gmail.com>
2020-02-28 12:21:39 +0900

    세팅

--- src/Pages/Header/Nav.js
+++ src/Pages/Header/Nav.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import LogoContainer from '../../Components/NavForm/LogoContainer';
-import MainNav from '../../Components/NavForm/MainNav';
-import '../../Styles/Nav.css';
+import React from "react";
+import LogoContainer from "../../Components/NavForm/LogoContainer";
+import MainNav from "../../Components/NavForm/MainNav";
+import "../../Styles/Nav.css";
 
 class Nav extends React.Component {
-
-    render(){
-        return( 
-          <header className = "header">
-            <LogoContainer />
-            <MainNav />
-          </header>
-        )
-    }
+  render() {
+    return (
+      <header className="header">
+        <LogoContainer />
+        <MainNav />
+      </header>
+    );
+  }
 }
 
 export default Nav;
--- src/index.js
+++ src/index.js
@@ -3,5 +3,4 @@ import ReactDOM from "react-dom";
 import Routes from "./Routes";
 import "./Styles/index.scss";
 
-
 ReactDOM.render(<Routes />, document.getElementById("root"));
