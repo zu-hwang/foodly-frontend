69a525fa146daddb139a4270881ff6bcd9764330
zu-hwang <tropical.kitsch@gmail.com>
2020-02-29 19:01:39 +0900

    로회원가입 유효검사 수정 및 이벤트 수정

--- src/Components/RegisterForm/RegisterForm.js
+++ src/Components/RegisterForm/RegisterForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
 import Input from "./Input";
 import Main from "../../Pages/Main/Main";
 import "../../Styles/accountForm.scss";
@@ -11,10 +12,15 @@ class Register extends Component {
       lastName: "",
       email: "",
       password: "",
-      checkForm: false
+      checkForm: false,
+      retry: false
     };
   }
+  goToHome = () => {
+    this.props.history.push("/");
+  };
   postData = () => {
+    // ! 요청보내기
     let myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
 
@@ -27,56 +33,78 @@ class Register extends Component {
 
     let requestOptions = {
       method: "POST",
-      // headers: myHeaders,
       body: raw
-      // redirect: "follow"
     };
 
     fetch("http://10.58.1.127:8000/account/signup", requestOptions)
-      .then((response) => response.text())
-      .then((result) => console.log("결과", result))
-      .catch((error) => console.log("error", error));
+      .then(response => response.json())
+      .then(result => {
+        console.log("결과", result);
+        if (result.acess) {
+          // ! 결과에 acess가 있으면, 200일때 다음 창으로 넘어가기
+          return this.goToHome();
+        } else if ("400일때") {
+          console.log("인풋 형식이 옳지 않음");
+          // ! 회원가입 등록이 안될경우 어떻게 할지
+          return this.setState({ retry: true });
+        } else if ("500일때") {
+          //  500page 띄우기
+        }
+      })
+      .catch(error => console.log("error", error));
   };
-  handleFirstNameValue = (e) => {
+  handleFirstNameValue = e => {
     this.setState({ firstName: e.target.value });
     // console.log(this.state.firstName);
   };
-  handleLastNameValue = (e) => {
+  handleLastNameValue = e => {
     this.setState({ lastName: e.target.value });
     // console.log(this.state.lastName);
   };
-  handleEmailValue = (e) => {
+  handleEmailValue = e => {
     this.setState({ email: e.target.value });
     // console.log(this.state.email);
   };
-  handlePasswordValue = (e) => {
+  handlePasswordValue = e => {
     this.setState({ password: e.target.value });
     // console.log(this.state.password);
   };
 
-  handleClickBtn = (e) => {
+  handleClickBtn = e => {
     this.setState({ checkForm: true });
     if (
-      this.state.firstName &&
-      this.state.lastName &&
-      this.state.email &&
-      this.state.password
+      this.state.firstName.length > 1 &&
+      !this.state.email.includes(" ") &&
+      this.state.lastName.length > 1 &&
+      !this.state.email.includes(" ") &&
+      this.state.email.indexOf("@") > 0 &&
+      this.state.email.indexOf(".") > 2 &&
+      !this.state.email.includes(" ") &&
+      this.state.password.length > 6 &&
+      !this.state.password.includes(" ")
     ) {
-      this.setState({ checkForm: true });
+      // this.setState({ checkForm: true });
       this.postData();
+      // console.log("입력이 완벽하군 ! 버튼을 클릭했으니 POST 요청!");
     }
   };
-  handleEnterKey = (e) => {
-    // console.log(e);
+  handleEnterKey = e => {
+    // console.log("엔터키 실행!");
     if (e.key === "Enter") {
       this.setState({ checkForm: true });
       if (
-        this.state.firstName &&
-        this.state.lastName &&
-        this.state.email &&
-        this.state.password
+        this.state.firstName.length > 1 &&
+        !this.state.email.includes(" ") &&
+        this.state.lastName.length > 1 &&
+        !this.state.email.includes(" ") &&
+        this.state.email.indexOf("@") > 0 &&
+        this.state.email.indexOf(".") > 2 &&
+        !this.state.email.includes(" ") &&
+        this.state.password.length > 6 &&
+        !this.state.password.includes(" ")
       ) {
         this.postData();
+        // console.log("입력이 완벽하군 ! ENTER를 눌렀으니 POST 요청!");
       }
     }
   };
@@ -85,6 +113,7 @@ class Register extends Component {
       checkForm: false
     });
   };
+
   render() {
     return (
       <Main>
@@ -132,16 +161,27 @@ class Register extends Component {
               OnChange={this.handlePasswordValue}
               OnKeyUp={this.handleEnterKey}
               CheckForm={this.state.checkForm}
+              OnFocus={this.handleOnFocus}
+              OnBlur={this.handleOnBlur}
             />
 
             <div className="btn-wrap">
+              <div
+                className={
+                  this.state.retry ? "notice-retry-block" : "notice-retry-none"
+                }
+              >
+                형식에 맞지 않음. 다시!
+              </div>
               <button type="button" onClick={this.handleClickBtn}>
                 re create my account
               </button>
             </div>
             <div className="text-container">
-              <p className="">
-                <span className="mid-red">Return to store</span>
+              <p>
+                <span className="mid-red" onClick={this.goToHome}>
+                  Return to store
+                </span>
               </p>
             </div>
           </div>
@@ -151,4 +191,4 @@ class Register extends Component {
   }
 }
 
-export default Register;
+export default withRouter(Register);
