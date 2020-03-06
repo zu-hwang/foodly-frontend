c7189afbc360bb6b1ec6e76bc375b84f2cb3098e
zu-hwang <tropical.kitsch@gmail.com>
2020-03-06 14:06:32 +0900

    번틀 장바구니 API 연결

--- src/Components/Bundle/Bundle.js
+++ src/Components/Bundle/Bundle.js
@@ -4,10 +4,22 @@ import "./Bundle.scss";
 
 class Bundle extends Component {
   onClick = e => {
+    // console.log(parseInt(e.target.id));
     // this.props.history.push("/");
+    fetch("http://10.58.1.167:8000/order/cart", {
+      method: "POST",
+      body: JSON.stringify({
+        id: parseInt(e.target.id)
+        // quantity: this.props.data
+      })
+    })
+      .then(res => res.json())
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
   };
   render() {
     const { data } = this.props;
+    console.log("번들", data);
     return (
       <div className="bundle-container">
         <div className="title-container">
@@ -23,6 +35,7 @@ class Bundle extends Component {
                 color={col[index]}
                 onClick={this.onClick}
                 key={index}
+                id={index}
               />
             );
           })}
